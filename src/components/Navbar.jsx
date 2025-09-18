bd8f4ebd03e47b47f6d720c4ade5cab5c09a0b74
Aishix7 <hithaishi714@gmail.com>
2025-09-18 13:07:56 +0530

    added proper styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,24 +9,24 @@ export const Navbar = () => {
             <div className="bg-blue-400 rounded-full p-2">diamond-logo</div>
             <span className="font-bold text-xl">E-Comm</span>
           </div>
+          <ul className="flex space-x-7">
+            <li>
+              <a href="/home">HOME</a>
+            </li>
+            <li>
+              <a href="/bag">BAG</a>
+            </li>
+            <li>
+              <a href="/sneakers">SNEAKERS</a>
+            </li>
+            <li>
+              <a href="/belt">BELT</a>
+            </li>
+            <li>
+              <a href="/contact">CONTACT</a>
+            </li>
+          </ul>
         </div>
-        <ul className="flex space-x-7">
-          <li>
-            <a href="/home">HOME</a>
-          </li>
-          <li>
-            <a href="/bag">BAG</a>
-          </li>
-          <li>
-            <a href="/sneakers">SNEAKERS</a>
-          </li>
-          <li>
-            <a href="/belt">BELT</a>
-          </li>
-          <li>
-            <a href="/contact">CONTACT</a>
-          </li>
-        </ul>
       </nav>
     </>
   );
