631c7a451640d008db43390a04000d4ebd4ec69e
Aishix7 <hithaishi714@gmail.com>
2025-09-18 22:30:13 +0530

    added new files Body.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import "./App.css";
 import { Navbar } from "./components/Navbar";
+import { Body } from "./components/Body";
 function App() {
   return (
     <>
       <Navbar />
+      <Body />
     </>
   );
 }
