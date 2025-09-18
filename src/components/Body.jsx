e4c4fa7b11a6ea773f771f29400012c543353c26
Aishix7 <hithaishi714@gmail.com>
2025-09-18 22:30:57 +0530

    created new files - Body.jsx , Products.jsx, SideBar.jsx

---
+++ src/components/Body.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Products } from "./Products";
+import { SideBar } from "./Sidebar";
+
+export const Body = () => {
+  return (
+    <>
+      <div className="flex h-screen container m-auto py-4">
+        <div className="w-1/4">
+          <SideBar />
+        </div>
+
+        <div className="w-3/4">
+          <Products />
+        </div>
+      </div>
+    </>
+  );
+};
